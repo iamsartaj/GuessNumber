194da344f7ab5d435778e349d8fd645264f94f62
iamsartaj <sartajahmadks@gmail.com>
2026-05-21 23:49:51 +0530

    score screen added

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from '../components/ui/PrimaryButton';
 import { useState } from 'react';
 import Colors from '../constants/colors';
 
